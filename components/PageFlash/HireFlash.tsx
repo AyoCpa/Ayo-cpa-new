a86f612ece958564df85d45af8afc2f6d875f125
Oluwaferanmi Jogbodo <feranmijogbodo@gmail.com>
2023-02-24 10:27:43 +0100

    fixes

--- components/PageFlash/HireFlash.tsx
+++ components/PageFlash/HireFlash.tsx
@@ -6,13 +6,13 @@ export const HireFlash = () => {
     return (
       <Row justify={"center"}  className="bg-[#6FB1E3] py-16">
         <Col xs={22} lg={20}>
-          <Row gutter={50} >
+          <Row >
             <Col xs={24} lg={8}>
               <div className={`${ubuntu.variable} font-bold font-ubuntu text-white text-xl lg:text-4xl`}>
                 Hire an Accountant / <br/> File your tax
               </div>
             </Col>
-            <Col xs={16} className="text-white "  >
+            <Col xs={24} lg={16} className="text-white "  >
               <div className={`${inter.variable} mt-3 lg:mt-0 text-sm font-inter font-medium  lg:text-lg`} >
                 Let us help you out with the receipts, the statements, the
                 forms, the loads of possible deductions and credits
