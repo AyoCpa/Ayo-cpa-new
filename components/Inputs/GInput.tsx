6fb52cab87dbde34640053dbe4e7f8c52a6a8cfb
Oluwaferanmi Jogbodo <feranmijogbodo@gmail.com>
2023-03-01 10:32:52 +0100

    fixed border on iphone

--- components/Inputs/GInput.tsx
+++ components/Inputs/GInput.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from "next/image"
 import {inter , ubuntu} from "@/utils/fonts"
+import styles from "./GInput.module.css"
 
 
 type GInputProps = {
@@ -14,7 +15,7 @@ function GInput({image , placeholder , name}:GInputProps) {
   const imageInputStyle = "absolute top-2.5 left-2";
   const inputStyle = `w-full py-3 px-12 placeholder-[#5A5A5A]  placeholder:text-xs lg:placeholder:text-base bg-transparent border-[#AAAAAA] border-2 rounded ${inter.variable} font-inter`;
   return (
-    <div className="relative mb-4 lg:mb-0">
+    <div className={` ${inter.variable} font-inter relative mb-4 lg:mb-0`}>
       <span className={imageInputStyle}>
         <Image src={image} alt="" />
       </span>
@@ -22,7 +23,7 @@ function GInput({image , placeholder , name}:GInputProps) {
       <input
         name={name}
         type="text"
-        className={inputStyle}
+        className={styles.inputStyle}
         placeholder={placeholder}
       />
     </div>
