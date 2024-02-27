e8a1803baee39ce035943608fc47796f8fd1f236
Joshua <jo4christ48@yahoo.com>
2024-02-27 21:48:43 +0100

    GA integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "antd/dist/antd";
 import store from "@/redux/store";
 import { Provider } from "react-redux";
 import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,8 @@ export default function App({ Component, pageProps }: AppProps) {
           <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
         </Head>
         <Component {...pageProps} />
+
+        <GoogleAnalytics gaId="=G-3XXWXW846N" />
       </Provider>
     </>
   );
