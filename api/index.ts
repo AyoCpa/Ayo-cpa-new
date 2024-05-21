9e311986bdf5df0462c632c413d71658ebec26f6
mark <feranmijogbodo@gmail.com>
2024-05-21 13:54:54 +0100

    authentication-page

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,30 @@
 import axios from "axios"
 import { toast } from "sonner"
 
-const api = axios.create({ baseURL: "http://localhost:4000/api" })
+const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_SERVER_URL })
+
+api.interceptors.request.use(function (config) {
+    // Get the token and assign to the request 
+
+    let token = localStorage.getItem("user-token");
+    if (token) {
+        const tokenParsed = JSON.parse(token);
+        config.headers['Authorization'] = `Bearer ${tokenParsed.access_token}`
+    }
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
 
 api.interceptors.response.use((response) => {
     return response
 }, (e) => {
+    console.log(e)
+    if (e?.response?.status == 401) {
+        localStorage.removeItem('user-token');
+        window.location.href = "/auth/sign-in"
+    }
     if (e?.response?.data?.message) {
         toast.error(e.response.data.message)
     } else {
