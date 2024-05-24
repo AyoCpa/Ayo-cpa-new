2cb4f43dc267e418e6da8e3121c51ebb6318799e
mark <feranmijogbodo@gmail.com>
2024-05-24 12:33:16 +0100

    adding logs

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_SERVER_URL })
 
 api.interceptors.request.use(function (config) {
     // Get the token and assign to the request 
-    console.log("process-ev" , process.env.NEXT_PUBLIC_SERVER_URL);
+    console.log("process-ev", process.env.NEXT_PUBLIC_SERVER_URL);
     let token = localStorage.getItem("user-token");
     if (token) {
         const tokenParsed = JSON.parse(token);
@@ -20,7 +20,6 @@ api.interceptors.request.use(function (config) {
 api.interceptors.response.use((response) => {
     return response
 }, (e) => {
-    console.log(e)
     if (e?.response?.status == 401) {
         localStorage.removeItem('user-token');
         window.location.href = "/auth/sign-in"
@@ -38,6 +37,9 @@ export const apiClient = async (method: 'get' | "post" | "delete" | "patch", url
         return await api.get(url);
     }
     else if (method.toLowerCase() === "post") {
+        console.log("Got inside the wait area");
+        console.log(body);
+        console.log(url)
         return await api.post(url, body);
     }
     else if (method.toLowerCase() === 'delete') {
